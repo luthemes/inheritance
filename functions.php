3628a1f65f6d3b7665ba50a9c991e48c121a6172
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-11-29 20:02:22 -0800

    remove the compatibility from functions.php

--- functions.php
+++ functions.php
@@ -1,6 +1,8 @@
 <?php
 /**
  * Default functions template
+ * 
+ * This file is used to bootstrap the theme.
  *
  * @package   Inheritance
  * @author    Benjamin Lu <benlumia007@gmail.com>
@@ -9,57 +11,13 @@
  * @link      https://github.com/benlumia007/inheritance
  */
 
-/**
- * Table of Content
- *
- * 1.0 - Compatibility Check
- * 2.0 - Backdrop Core
- */
+# ------------------------------------------------------------------------------
+# Load the composer files
+# ------------------------------------------------------------------------------
+#
+# Load the composer files. Note that autoloading should happen first so that
+# any classes or functions are available that we might need.
 
-/**
- * 1.0 - Compatibility Check
- */
-function inheritance_compatibility_check() {
-	if ( version_compare( $GLOBALS['wp_version'], '4.9', '<' ) ) {
-		return sprintf(
-			// translators: 1 =  a version string, 2 = current wp version string.
-			__( 'Initiator requires at least WordPress version %1$s. You are currently running %2$s. Please upgrade and try again.', 'inheritance' ),
-			'4.9',
-			$GLOBALS['wp_version']
-		);
-	} elseif ( version_compare( PHP_VERSION, '5.6', '<' ) ) {
-		return sprintf(
-			// translators: 1 =  a version string, 2 = current wp version string.
-			__( 'Initiator requires at least PHP version %1$s. You are currently running %2$s. Please upgrade and try again.', 'inheritance' ),
-			'5.6',
-			PHP_VERSION
-		);
-	}
-	return '';
-}
-
-/**
- * Triggered after switch themes and check if it meets the requirements.
- */
-function inheritance_switch_theme() {
-	if ( version_compare( $GLOBALS['wp_version'], '4.9', '<' ) || version_compare( PHP_VERSION, '5.6', '<' ) ) {
-		switch_theme( get_option( 'theme_switched' ) );
-		add_action( 'admin_notices', 'inheritance_upgrade_notice' );
-	}
-	return false;
-}
-add_action( 'after_switch_theme', 'inheritance_switch_theme' );
-
-/**
- * Displays an error if it doesn't meet the requirements.
- */
-function inheritance_upgrade_notice() {
-	printf( '<div class="error"><p>%s</p></div>', esc_html( inheritance_compatibility_check() ) );
-}
-
-/**
- * 2.0 - Backdrop Core
- */
 if ( file_exists( get_parent_theme_file_path( '/vendor/autoload.php' ) ) ) {
 	require_once get_parent_theme_file_path( '/vendor/autoload.php' );
 }
