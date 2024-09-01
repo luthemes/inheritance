ccb85db0b3fb486ee73481daeb69bfd496778fef
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-01 15:49:33 +0000

    re-commit

--- functions.php
+++ functions.php
@@ -6,18 +6,16 @@
  *
  * @package   Inheritance
  * @author    Benjamin Lu <benlumia007@gmail.com>
- * @copyright Copyright (C) 2022. Benjamin Lu
+ * @copyright 2022. Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
- * @link      https://github.com/benlumia007/inheritance
+ * @link      https://luthemes.com/portfolio/inheritance
  */
 
-# ------------------------------------------------------------------------------
-# Load the composer files
-# ------------------------------------------------------------------------------
-#
-# Load the composer files. Note that autoloading should happen first so that
-# any classes or functions are available that we might need.
-
-if ( file_exists( get_parent_theme_file_path( '/vendor/autoload.php' ) ) ) {
-	require_once get_parent_theme_file_path( '/vendor/autoload.php' );
-}
+/** ------------------------------------------------------------------------------------------
+ * Bootstrap the theme.
+ * -------------------------------------------------------------------------------------------
+ * Load the bootstrap files. Note that autoload should happen first so that any classes or
+ * functions are available that we might need.
+ */
+require_once get_parent_theme_file_path( 'app/bootstrap-autoload.php' );
+require_once get_parent_theme_file_path( 'app/framework.php' );
