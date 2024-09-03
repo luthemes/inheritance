7268c9127bc2e9a2aaf3af2f9e6c137c3894b546
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-03 17:27:55 +0000

    initial commti

---
+++ public/views/content/page.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Default page template
+ *
+ * @package   Inheritance
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright Copyright (C) 2022. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/inheritance
+ */
+?>
+<?php $engine = Benlumia007\Backdrop\App::resolve( 'view/engine' ); ?>
+<?php $engine->display( 'header' ); ?>
+	<section id="content" class="app-content">
+		<main id="main" class="app-main">
+			<?php while ( have_posts() ) : the_post(); ?>
+				<?php $engine->display( 'entry/page'  ); ?>
+			<?php endwhile; ?>
+			<?php comments_template(); ?>
+		</main>
+	</section>
+<?php $engine->display( 'footer' ); ?>
---
+++ public/views/content/single.php
@@ -0,0 +1,10 @@
+<section id="content" class="site-content">
+	<main id="main" class="content-area">
+		<?php if ( have_posts() ) : ?>
+			<?php while( have_posts() ) : the_post(); ?>
+				<?php Backdrop\View\display( 'entry/single' ); ?>
+			<?php endwhile; ?>
+				<?php Backdrop\View\display( 'nav/pagination', 'posts' ); ?>
+		<?php endif; ?>
+	</main>
+</section>
