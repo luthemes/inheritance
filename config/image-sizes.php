d057aa5072c202bd980cb4366f413e7b653c1ceb
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-03 01:16:06 +0000

    initial commit

---
+++ config/image-sizes.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Image Sizes Config.
+ *
+ * Defines the image sizes that the theme sets.
+ *
+ * @package   Inheritance
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2022 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/inheritance
+ */
+
+ return [
+
+	'inheritance-landscape-medium' => [
+		'label'  => __( 'Landscape: Medium', 'inheritance' ),
+		'width'  => 640,
+		'height' => 360
+	],
+	'inheritance-landscape-large' => [
+		'label'  => __( 'Landscape: Large', 'inheritance' ),
+		'width'  => 896,
+		'height' => 504
+	]
+];
