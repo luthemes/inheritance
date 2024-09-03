745b1f0024d69b3e2fe1c32841d6b3d042aa7785
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-03 17:05:55 +0000

    initial commit

---
+++ app/functions-assets.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Theme - Scripts
+ *
+ * @package   Inheritance
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2022 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/inheritance
+ */
+
+namespace Inheritance;
+  
+use function Backdrop\Mix\asset;
+
+/**
+ * Enqueue Scripts and Styles
+ *
+ * @since  1.0.0
+ * @access public
+ * @return void
+ *
+ * @link   https://developer.wordpress.org/reference/functions/wp_enqueue_style/
+ * @link   https://developer.wordpress.org/reference/functions/wp_enqueue_script/
+ */
+add_action( 'wp_enqueue_scripts', function() {
+
+	/**
+	 * Rather than enqueue the main stylesheet, we are going to enqueue sceen.css since all of the styles will
+	 * go here. We only need parse the information for the Theme in style.css so that it can be activated.
+	 */
+	wp_enqueue_style( 'inheritance-screen', asset( 'assets/css/screen.css' ), null, null );
+	wp_enqueue_script( 'inheritance-app', asset( 'assets/js/app.js' ), [ 'jquery' ], null, true );
+
+	/**
+	 * This allows users to comment by clicking on reply so that it gets nested.
+	 */
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+} );
