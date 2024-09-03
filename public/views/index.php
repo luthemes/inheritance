4687c1c9747a2437f910329294bd98c4844c3ff7
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-03 03:47:23 +0000

    initial commit

--- public/views/index.php
+++ public/views/index.php
@@ -1,28 +1,10 @@
 <?php
-/**
- * Default index template
- *
- * @package   Inheritance
- * @author    Benjamin Lu <benlumia007@gmail.com>
- * @copyright Copyright (C) 2022. Benjamin Lu
- * @license   https://www.gnu.org/licenses/gpl-2.0.html
- * @link      https://github.com/benlumia007/inheritance
- */
-?>
-<?php $engine = Benlumia007\Backdrop\App::resolve( 'view/engine' ); ?>
-<?php $engine->display( 'header' ); ?>
-	<section id="content" class="app-content">
-		<main id="main" class="app-main">
-			<?php
-				if ( have_posts() ) :
-					while ( have_posts() ) : the_post();
-						$engine->display( 'content', get_post_format()  );
-					endwhile;
-					the_posts_pagination();
-				else :
-						$engine->display( 'content/none' );
-				endif;
-			?>
-		</main>
-	</section>
-<?php $engine->display( 'footer' ); ?>
+
+// Loads header/*.php templates
+Backdrop\View\display( 'header', Backdrop\Template\Hierarchy\hierarchy() );
+
+// Loads content/*.php templates
+Backdrop\View\display( 'content', Backdrop\Template\Hierarchy\hierarchy() );
+
+// Loads footer/*.php templates
+Backdrop\View\display( 'footer', Backdrop\Template\Hierarchy\hierarchy() );
