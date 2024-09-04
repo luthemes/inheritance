13e3257d1f884fd9ac8aae992478dff51d1b9505
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-04 21:36:51 +0000

    initial commit

---
+++ public/views/entry/single.php
@@ -0,0 +1,23 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<header class="entry-header">
+		<?php Backdrop\Theme\Entry\display_title(); ?>
+		<div class="entry-metadata">
+			<?php Backdrop\Theme\Entry\display_date(); ?>
+		</div>
+	</header>
+	<div class="story-content">
+		<?php if ( has_post_thumbnail() ) { ?>
+			<picture class="post-thumbnail">
+			<?php
+				$size = get_theme_mod( 'theme_content_feature_image', 'inheritance-landscape-medium' ) ? get_theme_mod( 'theme_content_feature_image' ) : Mod::fallback( 'featured_image_size' );
+				the_post_thumbnail( $size );
+			?>				
+			</picture>
+		<?php } ?>
+		<div class="entry-content">
+			<?php the_content(); ?>
+			<?php Backdrop\View\display( 'nav/pagination', 'post' ); ?>
+		</div>	
+	</div>
+	<?php Backdrop\View\display( 'sidebar', 'primary', [ 'sidebar' => 'primary'] ); ?>
+</article>
