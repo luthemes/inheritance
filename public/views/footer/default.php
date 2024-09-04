29d4187b2d50a29c5c194d9892ef992c6a0fedab
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-04 18:29:34 +0000

    use custom displayCredit().

---
+++ public/views/footer/default.php
@@ -0,0 +1,9 @@
+	<footer id="colophon" class="site-footer">
+		<div class="site-info">
+			<?php Inheritance\Template\Footer::displayCredit(); ?>
+		</div>
+	</footer>
+</div>
+<?php wp_footer(); ?>
+</body>
+</html>
