c4b17d28eb543fc443b13c082115e81b40d9f42d
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2024-09-04 16:33:49 +0000

    small changes to naming.

--- app/Tools/PoweredBy.php
+++ app/Tools/PoweredBy.php
@@ -7,7 +7,7 @@
  *
  * @package   Inheritance
  * @author    Benjamin Lu <benlumia007@gmail.com>
- * @copyright 2024 Benjamin Lu
+ * @copyright 2022 Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
  * @link      https://luthemes.com/portfolio/inheritance
  */
