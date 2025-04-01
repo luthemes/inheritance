47841de5edf4751865a851619d705ccd7b61b304
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-04-01 18:45:31 +0000

    fixed featured images.

--- public/views/entry/single.php
+++ public/views/entry/single.php
@@ -2,17 +2,19 @@
 	<header class="entry-header">
 		<?php Backdrop\Theme\Entry\display_title(); ?>
 		<div class="entry-metadata">
-			<?php Backdrop\Theme\Entry\display_date(); ?>
+			<?php Backdrop\Theme\Entry\display_author(); ?>
+			<?php Backdrop\Theme\Entry\display_date( [ 'before' => Inheritance\Sep() ] ); ?>
 		</div>
 	</header>
 	<div class="entry-container">
 		<div class="story-content">
 			<?php if ( has_post_thumbnail() ) { ?>
 				<picture class="post-thumbnail">
-				<?php
-					$size = get_theme_mod( 'theme_content_feature_image', 'inheritance-landscape-medium' ) ? get_theme_mod( 'theme_content_feature_image' ) : Mod::fallback( 'featured_image_size' );
-					the_post_thumbnail( $size );
-				?>				
+					<?php
+						$size = get_theme_mod( 'theme_content_feature_image' );
+						$size = ! empty( $size ) ? $size : ( Inheritance\Tools\Mod::fallback( 'featured_image_size' ) ?: 'inheritance-landscape-medium' );
+						the_post_thumbnail( $size );
+					?>			
 				</picture>
 			<?php } ?>
 			<div class="entry-content">
