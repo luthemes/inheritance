fdb8305e11d3e34996795750f97f0c197302602e
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-07-16 18:38:54 +0000

    update and refine

--- app/framework.php
+++ app/framework.php
@@ -4,44 +4,55 @@
  *
  * @package   Inheritance
  * @author    Benjamin Lu <benlumia007@gmail.com>
- * @copyright Copyright (C) 2022. Benjamin Lu
+ * @copyright 2022 Benjamin Lu
  * @license   https://www.gnu.org/licenses/gpl-2.0.html
- * @link      https://github.com/benlumia007/inheritance
+ * @link      https://github.com/luthemes/inheritance
  */
 
-/**
- * Create a new framework instance
- *x
- * This will create an instance of the framework allowing you to initialize the theme.
- */
-$theme = Backdrop\booted() ? Backdrop\app() : new Backdrop\Core\Application();
+# ----------------------------------------------------------------------------------
+# Create a new application.
+# ----------------------------------------------------------------------------------
+#
+# This code creates the one true instance of the Backdrop Core Application, which can
+# be access via the `Backdrop\app();` function or the `Backdrop\App` static class after
+# the application has been booted.
+$theme = new Backdrop\Core\Application();
 
-/**
- * Register default providers.
- */
+# ------------------------------------------------------------------------------
+# Register default service providers with the application.
+# ------------------------------------------------------------------------------
+#
+# Here are the default service providers that are essential for the theme to function
+# before booting the application. These service providers form the foundation for the
+# theme.
+// $theme->provider( Backdrop\Customize\Provider::class );
 $theme->provider( Backdrop\Fonts\Provider::class );
-$theme->provider( Backdrop\Languages\Provider::class );
 $theme->provider( Backdrop\Mix\Provider::class );
-$theme->provider( Backdrop\Pagination\Provider::class );
-$theme->provider( Backdrop\Template\Hierarchy\Provider::class );
-$theme->provider( Backdrop\Template\Manager\Provider::class );
-$theme->provider( Backdrop\Theme\Provider::class );
-$theme->provider( Backdrop\View\Provider::class );
 
 /**
  * Register custom providers for the theme.
  */
+
 $theme->provider( Inheritance\Provider::class );
 $theme->provider( Inheritance\Customize\Provider::class );
 $theme->provider( Inheritance\FeatureImage\Provider::class );
 $theme->provider( Inheritance\Settings\Provider::class );
 
-/**
- * Create an action hook for child themes.
- */
+# ------------------------------------------------------------------------------
+# Perform bootstrap actions.
+# ------------------------------------------------------------------------------
+#
+# This code creates an action hook that child themes or plugins can utilize to
+# integrate their own binding into the bootstrap process before the app is booted.
+# The action callback receives the application instance as a parameter.
+
 do_action( 'inheritance/child/theme', $theme );
 
-/**
- * Boot the Framework
- */
+# ------------------------------------------------------------------------------
+# Bootstrap the application.
+# ------------------------------------------------------------------------------
+#
+# The code invokes the `boot();` method of the application, which initiates the
+# launch of the application. Congratulations on a job well done!
+
 $theme->boot();
