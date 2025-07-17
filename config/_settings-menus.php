4d5ea2405e25ce1618089dfa82d8d9b2a3a6399e
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-07-17 15:55:03 +0000

    initial commit

---
+++ config/_settings-menus.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Inheritance Menu Settings Configuration.
+ *
+ * This configuration file defines the menu locations for the Inheritance theme.
+ * Each menu location includes a name and is registered during theme setup.
+ *
+ * @package   Inheritance
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2022 Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://luthemes.com/portfolio/inheritance
+ */
+
+return [
+    'primary' => [
+        'name' => __( 'Primary Navigation', 'inheritance' ),
+    ],
+    'social' => [
+        'name' => __( 'Social Navigation', 'inheritance' ),
+    ]
+];
