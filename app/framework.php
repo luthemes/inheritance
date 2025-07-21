c2eb49ab1c595f7c22872090f60e58a75a056ea1
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-07-21 21:08:21 +0000

    readd customizer from backdrop defaults.

--- app/framework.php
+++ app/framework.php
@@ -25,7 +25,7 @@ $theme = new Backdrop\Core\Application();
 # Here are the default service providers that are essential for the theme to function
 # before booting the application. These service providers form the foundation for the
 # theme.
-// $theme->provider( Backdrop\Customize\Provider::class );
+$theme->provider( Backdrop\Customize\Provider::class );
 $theme->provider( Backdrop\Fonts\Provider::class );
 $theme->provider( Backdrop\Mix\Provider::class );
 
@@ -35,8 +35,8 @@ $theme->provider( Backdrop\Mix\Provider::class );
 
 $theme->provider( Inheritance\Provider::class );
 $theme->provider( Inheritance\Customize\Provider::class );
+$theme->provider( Inheritance\Image\Provider::class );
 $theme->provider( Inheritance\Menu\Provider::class );
-$theme->provider( Inheritance\FeatureImage\Provider::class );
 $theme->provider( Inheritance\Settings\Provider::class );
 $theme->provider( Inheritance\Sidebar\Provider::class );
 
