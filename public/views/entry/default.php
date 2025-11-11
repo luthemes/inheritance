f297fecfb28cf44a74538738d310b5ae700d7c9f
Benjamin Lu <164923145+techkodeninja@users.noreply.github.com>
2025-11-11 17:16:44 +0000

    update file

--- public/views/entry/default.php
+++ public/views/entry/default.php
@@ -1,21 +1,19 @@
-<li class="grid-item">
+<li class="grid-item <?php echo ( 0 === $GLOBALS['wp_query']->current_post && ! is_paged() ) ? 'is-featured' : ''; ?>">
 	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php if ( has_post_thumbnail() ) { ?>
 			<picture class="post-thumbnail">
 				<?php the_post_thumbnail( 'inheritance-landscape-small' ); ?>
-				<?php the_post_thumbnail( 'inheritance-landscape-medium' ); ?>
 			</picture>
 		<?php } ?>
 		<header class="entry__header">
+			<?php Backdrop\Post\display_date(); ?>
 			<?php Backdrop\Post\display_title(); ?>
-			<div class="entry__metadata">
-				<?php Backdrop\Post\display_author( [ 'before' => Inheritance\Tools\Svg::display( 'meta-icons', 'user' ) ] ); ?>
-				<?php Backdrop\Post\display_date( [ 'before' => Inheritance\Tools\Svg::display( 'meta-icons', 'calendar' ) ] ); ?>
-				<?php Backdrop\Post\display_comments_link( [ 'before' => Inheritance\Tools\Svg::display( 'meta-icons', 'comment' ) ] ); ?>
-			</div>
 		</header>
 		<div class="entry__content">
 			<?php the_excerpt(); ?>
 		</div>
+			<div class="entry__metadata">
+				<?php Backdrop\Post\display_author(); ?>
+			</div>
 	</article>
 </li>
